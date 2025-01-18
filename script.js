f003ec71c7b32f1d05aef27451d81d734fbe6bb0
salmakhatun1234umme <ummesalma00569@gmail.com>
2025-01-18 11:48:14 +0600

    added hover

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ function loadUser(){
             const userCard=document.createElement('div')
             userCard.classList.add('user-card');
 
-            userCard.innerHTML=`<div class='image'><img src="${user.image}"></div> <div class='discription'><h1> ${user.name}</h1><h3> ${user.profession}</h3> <h3>Age: ${user.age}</h3>  
+            userCard.innerHTML=`<div class='image'><img src="${user.image}"></div> <div class='discription'> 
+             <h1> ${user.name}</h1><h3> ${user.profession}</h3> <h3>Age: ${user.age}</h3>  
               
               <p > Email:${user.email}</p></div>`
             userContainer.appendChild(userCard)
