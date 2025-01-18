7084fb0575f69e47ac5e10a6c5b2b2a53d6e563c
salmakhatun1234umme <ummesalma00569@gmail.com>
2025-01-18 10:53:03 +0600

    added style

--- script.js
+++ script.js
@@ -7,8 +7,9 @@ function loadUser(){
             const userCard=document.createElement('div')
             userCard.classList.add('user-card');
 
-            userCard.innerHTML=`<img src="${user.image}"> <h1> ${user.name}</h1> <h3>Age: ${user.age}</h3>  
-              <p> Email:${user.email}</p>`
+            userCard.innerHTML=`<div class='image'><img src="${user.image}"></div> <div class='discription'><h1> ${user.name}</h1><h3> ${user.profession}</h3> <h3>Age: ${user.age}</h3>  
+              
+              <p > Email:${user.email}</p></div>`
             userContainer.appendChild(userCard)
             console.log(userContainer)
 
